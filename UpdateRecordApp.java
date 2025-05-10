704224157d90ee452b6facef0f87ae7b62d5a619
prateekbanra7 <66116227+prateekbanra7@users.noreply.github.com>
2025-05-10 11:39:22 +0530

    Add files via upload

---
+++ UpdateRecordApp.java
@@ -0,0 +1,47 @@
+package in.abc.main;
+
+import java.sql.SQLException;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import in.abc.util.HibernateUtil;
+
+public class UpdateRecordApp {
+
+	public static void main(String[] args) {
+
+		Session session = null;
+		Transaction transaction = null;
+		int count = 0;
+		boolean flag = false;
+
+		try {
+			session = HibernateUtil.getSession();
+			transaction = session.beginTransaction();
+
+			Query query = session
+					.createQuery("update in.abc.Model.Employee SET esalary = esalary + :increment WHERE eno <= :id");
+
+			query.setParameter("increment", 10000);
+			query.setParameter("id", 18);
+
+			count = query.executeUpdate();
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (flag) {
+				transaction.commit();
+				System.out.println("No of records updated is ::" + count);
+			} else {
+				transaction.rollback();
+			}
+			HibernateUtil.closeSession(session);
+			HibernateUtil.closeSessionFactory();
+		}
+
+	}
+
+}
